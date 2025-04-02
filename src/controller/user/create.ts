e4ecaf1980f2da21bb455c88e4cbdd2ecd43aa45
felipeluis223 <lf9072962@gmail.com>
2025-04-01 21:27:49 -0300

    feat: aplicando verificação se usuário já está cadastrado.

--- src/controller/user/create.ts
+++ src/controller/user/create.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from "express";
 import User from "../../database/models/User";
 
-const createUser = async (req: Request, res: Response)=>{
-    try{
+const createUser = async (req: Request, res: Response) => {
+    try {
         const { name, email, password } = req.body;
+
+        // Verifica se o e-mail já está cadastrado
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(409).json({ message: "Usuário já cadastrado com este e-mail." });
+        }
+
+        // Cria um novo usuário
         const newUser = await User.create({ name, email, password });
-        res.status(201).json(newUser);
-    }catch(error){
-        res.status(500).json({ message: "Falha! Verifique suas informações e tente novamente."});
+        return res.status(201).json(newUser);
+    } catch (error) {
+        return res.status(500).json({ message: "Falha! Verifique suas informações e tente novamente." });
     }
 };
 
