356ad55aa4ff0bc8f873d0ba088e88216e567602
felipeluis223 <lf9072962@gmail.com>
2025-04-27 11:29:05 -0300

    chore: alterando a rota de usuário - arquivos.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 import sequelize from "./database/database";
 import { login } from "./auth/authController";
 import { authenticate } from "./auth/authMiddleware";
-import createUser from "./controller/user/create";
-import getUser from "./controller/user/getAll";
 import createEmployee from "./controller/employee/create";
 import getEmployee from "./controller/employee/getAll";
 import { googleLogin } from "./auth/googleLogin";
+
+import useRoutes from "./routes/user";
 
 // Obter os arquivos .env:
 dotenv.config();
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
     res.send("Welcome in BETA");
 });
 
-// Login público
+// Autenticação de login público:
 app.post("/login", async (req, res) => {
     await login(req, res);
 });
@@ -40,14 +40,8 @@ app.post("/login", async (req, res) => {
 // Autenticação de login - Google:
 app.post("/auth/google", googleLogin);
 
-// Rotas protegidas:
-app.post("/users", async (req, res) => {
-    await createUser(req, res);
-});
-
-app.get("/users", authenticate, async (req, res) => {
-    await getUser(req, res);
-});
+// // Rotas protegidas de usuários:
+app.use('/users', useRoutes);
 
 app.post("/employees", authenticate, async (req, res) => {
     await createEmployee(req, res);
---
+++ src/routes/user/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import createUser from '../../controller/user/create';
+import { authenticate } from '../../auth/authMiddleware';
+import getUsers from '../../controller/user/getAll';
+
+const router = Router();
+
+// Rotas protegidas
+router.post('/', async (req, res) => {
+    await createUser(req, res);
+});
+
+router.get('/', authenticate, async (req, res) => {
+    await getUsers(req, res);
+});
+
+export default router;
